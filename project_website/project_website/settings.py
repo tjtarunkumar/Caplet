387e96a9059de327319bc1fca5c6a0e56b49d6f2
tjtarunkumar <jyotitarun280@gmail.com>
2022-09-14 11:43:47 +0530

    setting file

--- project_website/project_website/settings.py
+++ project_website/project_website/settings.py
@@ -160,7 +160,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_ROOT = BASE_DIR / 'static'
 
 
 # Default primary key field type
